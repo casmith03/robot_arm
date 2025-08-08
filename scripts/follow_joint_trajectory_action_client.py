3efee7ff96e8b7615ed540aac8ded39d1ee80309
charlotte <c.amaris0003@gmail.com>
2025-08-08 18:33:28 +0100

    added joint tests and implemented cancel goal (stops robot)

--- scripts/follow_joint_trajectory_action_client.py
+++ scripts/follow_joint_trajectory_action_client.py
@@ -51,7 +51,8 @@ class FollowJointTrajectoryActionClient(Node):
 
         self.arm_joints = ["joint1", "joint2", "joint3", "joint4"]
         self.current_positions = [0.0] * len(self.arm_joints) # for feedback??
-        self.joint_states_received = False # TODO: what does this do?
+        self.formatted_current_positions_deg = [0.0] * len(self.arm_joints)
+        self.joint_states_received = False
 
         self.current_goal_handle = None
         self.get_result_future = None
@@ -67,33 +68,71 @@ class FollowJointTrajectoryActionClient(Node):
         
         ## arnavs code -------------------
 
-        self.get_logger().info("Follow Joint Trajectory node initialized")
+        print()
+        self.get_logger().info("==================================================")
+        self.get_logger().info("Follow Joint Trajectory Action Client initialized.")
+        self.get_logger().info("==================================================")
 
 
     # Credit: Robotics Assignment 
-    def on_shutdown(self):
+    def on_shutdown(self, future):
         """Custom behaviour on shutdown."""
         self.get_logger().info("Stopping the robot...")
-        # TODO: publish commands to stop all motion - option to reutrn to initial joint states?? or does it do this already?
+
+        # cancel goal and stop motion
+        goal_handle = future.result()
+        self.cancel_goal(goal_handle)
+
         self.shutdown = True
+    
+    
+    # stops robot motion
+    def cancel_goal(self, goal_handle):
+        self.get_logger().info("Cancelling goal...")
+        cancel_goal = goal_handle.cancel_goal_async() # async - doesn't pause execution
+        cancel_response = goal_handle.get_result_async() 
+
+        self.get_logger().info("Goal cancelled.")
 
 
     # Credit: Arnavs
-    # TODO: what is this useful for?
-    # poupulates initial joint states - only once?
+    # prints initial joint states
+    # continually updtaes current joint positions
     def joint_state_callback(self, msg: JointState):
+        # continually updtaes current positions
         for i, name in enumerate(self.arm_joints):
             if name in msg.name:
                 idx = msg.name.index(name)
-                self.current_positions[i] = msg.position[idx]
-        
+                self.current_positions[i] = msg.position[idx] # [rad]
+                self.formatted_current_positions_deg[i] = round(math.degrees(msg.position[idx]), 1) # [deg], 1 dp
+
         if not self.joint_states_received:
             self.joint_states_received = True
-            # self.get_logger().info("Initial joint states received")
+            self.get_logger().info("Initial joint states received.")
+            # self.get_logger().info(f"Initial joint states (radians): {self.current_positions}")
+            self.get_logger().info(f"Initial joint states (deg): {self.formatted_current_positions_deg}")
 
-        # print initial joint states
+
+    # Feedback
+    # from both action feedback and joint states
+    # only prints while the goal is active
+    def feedback_callback(self, feedback_msg):
+        feedback = feedback_msg.feedback
+
+        # print feedback parameters (current trajectory)
         if self.debug_mode:
-            self.get_logger().info(f"{self.current_positions}")
+            self.get_logger().info(
+                f"\n\n---- FEEDBACK ----\n"
+                f"Action in progress...\n"
+                f"Current joint states: {feedback.actual}\n" # TODO: custom status display?? AND add rate limit ... make universal var?
+                , throttle_duration_sec=1.0
+                )
+        
+        # print current joint states while goal is active (should be same as above feedback)
+        self.get_logger().info(
+            f"Current joint states (deg): {self.formatted_current_positions_deg}\n"
+            , throttle_duration_sec=0.5
+            ) # every x seconds # TODO: custom status display??
 
 
     # # Credit: Arnavs
@@ -112,7 +151,7 @@ class FollowJointTrajectoryActionClient(Node):
 
 
 
-    #  update params to 'journey' ?? or should i make the journey multiple goals, ie one goals = one waypoint on the journey??
+    # update params to 'journey' ?? or should i make the journey multiple goals, ie one goals = one waypoint on the journey??
     """
         # joint trajectory points float64[]
         # Each 'JointTrajectoryPoint()' describes where all joints (from trajectory.joint_names) should be at a specific time (from start) aka 'time_from_start'.
@@ -155,21 +194,26 @@ class FollowJointTrajectoryActionClient(Node):
         if self.current_goal_handle is not None:
             self.current_goal_handle.cancel_goal_async()
     
+
         goal_msg = FollowJointTrajectory.Goal()
 
         # joint names string[]
         goal_msg.trajectory.joint_names = self.arm_joints # add joints to goal_msg
 
-        point1 = JointTrajectoryPoint()
-        point1.positions = [0.0, 0.0, 0.0, 0.0] # Start (4 joints) # moves to start pos first every time new goal is sent 
-        point1.time_from_start.sec = 2
+        start = JointTrajectoryPoint()
+        start.positions = [0.0, 0.0, 0.0, 0.0] # Start (4 joints)
+        start.time_from_start.sec = 2 # 1s to get to start
 
-        point2 = JointTrajectoryPoint()  
+        waypoint_1 = JointTrajectoryPoint()  
         # point2.positions = [, , , ] # joint angles in rad - TODO: weave in joint limits?? - ASK ARNAV 
-        point2.positions = joint_angles
-        point2.time_from_start.sec = 3 # Move here in 2s
+        waypoint_1.positions = joint_angles
+        waypoint_1.time_from_start.sec = start.time_from_start.sec + 2 # Move here in 2s
 
-        goal_msg.trajectory.points = [point1, point2] # the journey
+        end = JointTrajectoryPoint()
+        end.positions = [0.0, 0.0, 0.0, 0.0] # Start (4 joints)
+        end.time_from_start.sec = waypoint_1.time_from_start.sec + 2
+
+        goal_msg.trajectory.points = [waypoint_1] # the journey - moves from start back to start
 
         # Sending goal to action server
         future = self._action_client.send_goal_async(goal_msg, self.feedback_callback)
@@ -203,21 +247,7 @@ class FollowJointTrajectoryActionClient(Node):
     #     return [joint1, joint2, joint3, joint4]
 
 
-    # Feedback
-    def feedback_callback(self, feedback_msg):
-        feedback = feedback_msg.feedback
-
-        # print feedback parameters (current trajectory)
-        if self.debug_mode:
-            self.get_logger().info(
-                f"\n\n---- FEEDBACK ----\n"
-                f"Action in progress...\n"
-                f"Trajectory: \n"
-                f"  {feedback.actual}\n" # TODO: custom status display?? AND add rate limit ... make universal var?
-                )
-
-
-    # informs if server accepts the goal or not
+    # Informs if server accepts the goal or not
     def goal_response_callback(self, future):
         goal_handle = future.result() # future.result() (stored in goal_handle) is the object returned after sending goal to server. Its fields reflect the fields in the action structure.
         if not goal_handle.accepted:
@@ -234,36 +264,27 @@ class FollowJointTrajectoryActionClient(Node):
 
 
     # Processes server reponse
-    # Prints result to temrinal # TODO: make debugging mode??
+    # Prints result to temrinal # TODO: make debugging mojoint_angles1de??
     def get_result_callback(self, future):
         result = future.result().result # get result 
-
-        # TODO: more feedback - error code, error msg, actual trajectory??
-        self.get_logger().info(
-            f"\n=== RESULT ===\n"
-            f"Action Completed."
-            # f"Trajectory: \n"
-            # f"  {result.actual}\n" # TODO: custom status display??
-            )
-
         self.goal_succeeded = True
 
+        # # TODO: more feedback - error code, error msg, actual trajectory??
+        self.get_logger().info(f"Action Completed.")
+        # self.get_logger().info(f"Final joint states (radians): {self.current_positions}\n") 
+        self.get_logger().info(f"Final joint states (deg): {self.formatted_current_positions_deg}\n") # TODO: custom status display??
 
 
-# DEFAULT MAIN from webiste 
-# if __name__ == '__main__':
-#     rclpy.init(args=args)
+    # ----- HELPER METHODS -------
 
-#     action_client = FollowJointTrajectoryActionClient()
-
-#     # TODO: adapt to your action's parameters
-#     future = action_client.send_goal(a, b)
-
-#     rclpy.spin_until_future_complete(action_client, future)
-
-#     rclpy.shutdown()
-
-
+    def print_format_goal(self, joint_angles):
+        self.get_logger().info(f"Sending goal...")
+        self.get_logger().info(f"--------- Goal: --------")
+        n = 0
+        for joint_name in self.arm_joints:
+            self.get_logger().info(f"   {joint_name}: {math.degrees(joint_angles[n]):.1f} degrees")
+            n += 1
+        self.get_logger().info(f"------------------------")
 
 
 # OWN main method
@@ -277,21 +298,45 @@ def main(args=None):
     try:
         # while rclpy.ok:
         # # while not action_client.shutdown: # works the same as while clause above 
-        #     rclpy.spin_once(action_client, timeout_sec=0.1) # does this control flow behave as expected?
+        #     rclpy.spin_once(action_client, timeout_sec=0.1)
 
-        # TODO: adapt to your action's parameters
-        joint_angles1 = [math.radians(30),0.0,0.0,0.0] # [rad] # (waypont 1)
-        future = action_client.send_goal(joint_angles1)
+        # joint1 = # base
+        # joint2 = # shoulder
+        # joint3 = # elbow
+        # joint4 = # wrist
+        # gripper = 0
+
+
+        # articulate each joint
+        # [rad] # [+ clockwise, + down, + down, + down]
+        joint_angles = [math.radians(-30),0.0,0.0,0.0] # (-) anticlockwise
+        joint_angles = [0.0,math.radians(10),0.0,0.0] # (+) down
+        joint_angles = [0.0,0.0,math.radians(20),0.0] # (+) down
+        joint_angles = [0.0,0.0,0.0,math.radians(-20)] # (-) up
+
+        # combine joint movements
+        joint_angles = [math.radians(-20),math.radians(-20),math.radians(-20),math.radians(-20)] # (-) moves up and anticlockwise 
+        # joint_angles = [math.radians(20),math.radians(20),math.radians(20),math.radians(20)] # (+) moves down and clockwise 
+
+        # sending multiple goals ...
+        # make square
+        start = [0.0,0.0,0.0,0.0] # [rad] 
+        waypoint_1 = [-math.radians(30),0.0,0.0,0.0] # [rad] 
+
+
+
+
+        action_client.print_format_goal(joint_angles) # print goal 
+        future = action_client.send_goal(joint_angles)
         # rclpy.spin_until_future_complete(action_client, future) # spins until goal reponds (future), then shuts down. Error - doesnt allow any feedback as shuts node right away. try inseatd of rclpy.spin(action_client) - continuous
-
-        rclpy.spin(action_client) # fixes feedback bug 
+        rclpy.spin(action_client) # fixes feedback bug - doesn't stop node prematurely like above 'spin_until_future_complete'
 
     except KeyboardInterrupt:
         print(
             f"{action_client.get_name()} received a shutdown request (Ctrl+C)."
         )
     finally:
-        action_client.on_shutdown()
+        action_client.on_shutdown(future)
         while not action_client.shutdown:
             continue
         action_client.destroy_node()
